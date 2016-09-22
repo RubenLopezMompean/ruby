38b262297fc8484062450d80daf1092145588150
RubenLopezMompean <rlmompean@gmail.com>
2016-09-22 10:32:54 +0200

    probando ruby

---
+++ holamundo.rb
@@ -0,0 +1,17 @@
+puts "hola mundo "
+print "como te llamas "
+nombre = gets.chomp
+puts "que nombre más chulo"
+puts "je" * 3
+print " " * 4
+puts "*"
+print " " * 3
+puts "*" * 2
+print " " * 2
+puts "*" * 3
+print " " * 1
+puts "*" * 4
+puts "*" * 5
+print "te gusta mi triángulo"
+triangulo = gets.chomp
+puts "bueno, intentaré mejorarlo"
